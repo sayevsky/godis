d0a02ee791ca1dcef6afa4748517e35d4b4fa0fb
Viacheslav Sayevsky <sayevsky@gmail.com>
2016-09-25 20:43:13 +0200

    serialisation

--- server/storage.go
+++ server/storage.go
@@ -4,7 +4,11 @@ import (
 	"regexp"
 	"time"
 	"fmt"
+	"strconv"
+	"bytes"
 )
+
+type OK byte
 
 type Response struct {
 	Result interface{}
@@ -17,8 +21,77 @@ type WrappedValue struct {
 	TTL time.Time
 }
 
-func (Response) Serialize() ([]byte){
-	return []byte("+OK")
+func (r Response) Serialize() ([]byte){
+	// start with
+	// error: -\r\n<numberOfBytes>\r\n<message>
+	// success with result: +\r\n<result>
+	// if result starts with @ it's a string +\r\n@<numberOfBytes>\r\n<mesage>
+	// if result starts with * it's an array
+	// 	+\r\n*<numberOfElements>\r\n<sizeOfFirstElement>\r\n<FirstElement>\r\n...<sizeOfLastElement>\r\n<LastElement>\r\n
+	// if result starts with > it's an dict
+	// +\r\n*<numberOfElements>\r\n<sizeOfFirstElement>\r\n<FirstElement>\r\n...<sizeOfLastElement>\r\n<LastElement>\r\n
+	// if result starts with $ it's int $\r\n<number>\r\n
+
+	var buffer bytes.Buffer
+
+	if r.err != nil {
+		size := len(r.err.Error())
+		buffer.WriteString("-\r\n")
+		buffer.WriteString(strconv.Itoa(size))
+		buffer.WriteString("\r\n@")
+		buffer.WriteString(r.err.Error())
+		buffer.WriteString("\r\n")
+		return buffer.Bytes()
+	}
+
+	switch value := r.Result.(type) {
+	case string:
+		buffer.WriteString("+\r\n")
+		buffer.WriteString(strconv.Itoa(len(value)))
+		buffer.WriteString("\r\n@")
+		buffer.WriteString(value)
+		buffer.WriteString("\r\n")
+	case []string:
+		buffer.WriteString("+\r\n*")
+		buffer.WriteString(strconv.Itoa(len(value)))
+		buffer.WriteString("\r\n")
+		for _, element := range value {
+			buffer.WriteString(strconv.Itoa(len(element)))
+			buffer.WriteString("\r\n")
+			buffer.WriteString(element)
+			buffer.WriteString("\r\n")
+		}
+	case map[string]string:
+		buffer.WriteString("+\r\n>")
+		buffer.WriteString(strconv.Itoa(len(value)))
+		buffer.WriteString("\r\n")
+		for k,v := range value {
+			buffer.WriteString(strconv.Itoa(len(k)))
+			buffer.WriteString("\r\n")
+			buffer.WriteString(k)
+			buffer.WriteString("\r\n")
+			buffer.WriteString(strconv.Itoa(len(v)))
+			buffer.WriteString("\r\n")
+			buffer.WriteString(v)
+			buffer.WriteString("\r\n")
+		}
+	case int:
+		buffer.WriteString("+\r\n")
+		buffer.WriteString(strconv.Itoa(value))
+		buffer.WriteString("\r\n")
+	case OK:
+		buffer.WriteString("+\r\nOK\r\n")
+
+	default:
+		message := "Unxpected type"
+		buffer.WriteString("-\r\n")
+		buffer.WriteString(strconv.Itoa(len(message)))
+		buffer.WriteString("\r\n@")
+		buffer.WriteString(message)
+		buffer.WriteString("\r\n")
+	}
+
+	return buffer.Bytes()
 }
 func (w WrappedValue) IsZero() (bool){
 	return w.Value == nil && w.TTL.IsZero()
@@ -73,13 +146,9 @@ func ProcessCommands(dbCannel chan interface{}, withActiveEviction bool) {
 			}
 			ttl := durationToTTL(command.duration)
 			wrappedValue := &WrappedValue{command.Value, ttl}
-			old := storage[command.Key]
-			var res interface{}
-			if old != nil {
-				res = old.Value
-			}
 			storage[command.Key] = wrappedValue
-			command.Base.ChannelWithResult <- Response{res, nil}
+
+			command.Base.ChannelWithResult <- Response{OK(0), nil}
 		case *Get:
 			value := storage[command.Key]
 			//passive eviction
