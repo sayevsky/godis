8840a56c2aa555a042abdedda6f779a4c9ad9736
Viacheslav Sayevsky <sayevsky@gmail.com>
2016-09-25 22:28:10 +0200

    starting with client

---
+++ client/client.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	"net"
+	"bytes"
+	"strconv"
+	"bufio"
+)
+
+func NewClient(addr string) (Client, error) {
+	conn, err := net.Dial("tcp", addr)
+	return Client{conn}, err
+}
+
+type Client struct {
+	conn net.Conn
+}
+
+func (c Client) Get(key string) (interface{}, error) {
+	// <command>\r\n<numberOfBytesOfValue>\r\n<key>\r\n
+	var buffer bytes.Buffer
+	buffer.WriteString("GET\r\n")
+	buffer.WriteString(strconv.Itoa(len(key)))
+	buffer.WriteString("\r\n" + key + "\r\n")
+	c.conn.Write(buffer.Bytes())
+	result := bufio.NewReader(c.conn)
+	resType, err := result.ReadByte()
+	return string(resType), err
+
+}
---
+++ client/client_test.go
@@ -0,0 +1,13 @@
+package client
+
+import "testing"
+
+func Test(t *testing.T) {
+	//client, _ := NewClient("localhost:6380")
+
+	//res, err := client.Get("a")
+	//if res != "-"{
+	//	t.Errorf("fail to get a key", res, err)
+	//}
+
+}
