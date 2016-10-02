b50fc1739516839e5120737f4cfecf82205a3b9d
Viacheslav Sayevsky <sayevsky@gmail.com>
2016-10-02 18:05:27 +0200

    ttl as parameter

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"github.com/sayevsky/godis/internal"
 	"net"
+	"time"
 )
 
 func NewClient(addr string) (Client, error) {
@@ -18,42 +19,43 @@ type Client struct {
 func (c Client) Get(key string) *internal.Response {
 	request, _ := internal.Get{key, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
-func (c Client) Set(key string, value interface{}) *internal.Response {
-	request, _ := internal.SetUpd{key, value, 0, false, internal.BaseCommand{false, nil}}.Serialize()
+func (c Client) Set(key string, value interface{}, ttl time.Duration) *internal.Response {
+	request, _ := internal.SetUpd{key, value, ttl, false, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
-func (c Client) Update(key string, value interface{}) *internal.Response {
-	request, _ := internal.SetUpd{key, value, 0, true, internal.BaseCommand{false, nil}}.Serialize()
+
+func (c Client) Update(key string, value interface{}, ttl time.Duration) *internal.Response {
+	request, _ := internal.SetUpd{key, value, ttl, true, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
 func (c Client) Delete(key string) *internal.Response {
 	request, _ := internal.Del{key, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
 func (c Client) Keys(pattern string) *internal.Response {
 	request, _ := internal.Keys{pattern, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
 func (c Client) Count() *internal.Response {
 	request, _ := internal.Count{internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
@@ -61,7 +63,7 @@ func (c Client) Count() *internal.Response {
 func (c Client) GetIth(key string, index int) *internal.Response {
 	request, _ := internal.GGetI{key, index, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
 }
 
@@ -69,6 +71,10 @@ func (c Client) GetIth(key string, index int) *internal.Response {
 func (c Client) GetKeyInValue(key string, keyInValue string) *internal.Response {
 	request, _ := internal.GGetK{key, keyInValue, internal.BaseCommand{false, nil}}.Serialize()
 	c.conn.Write(request)
-	response := bufio.NewReader(c.conn)
+	response := readResponse(c.conn)
 	return internal.DeserializeResponse(response)
+}
+
+func readResponse(conn net.Conn) *bufio.Reader {
+	return bufio.NewReaderSize(conn, 64)
 }
--- client/client_test.go
+++ client/client_test.go
@@ -5,6 +5,9 @@ import (
 	"reflect"
 	"sort"
 	"testing"
+	"strconv"
+	"log"
+	"time"
 )
 
 func TestGetEmpty(t *testing.T) {
@@ -13,7 +16,7 @@ func TestGetEmpty(t *testing.T) {
 	client, _ := NewClient("localhost:6380")
 	res := client.Get("a")
 	if res.Err.Error() != "NE" {
-		t.Errorf("fail to get a key", res)
+		t.Error("fail to get a key", res)
 	}
 	s.Stop()
 }
@@ -22,10 +25,10 @@ func TestUpdateGet(t *testing.T) {
 	s := server.NewServer()
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
-	res := client.Update("a", "b")
+	res := client.Update("a", "b", 10 * time.Second)
 	res = client.Get("a")
 	if res.Err == nil {
-		t.Errorf("managed to get a key though it should not exist", res)
+		t.Error("managed to get a key though it should not exist", res)
 	}
 	s.Stop()
 }
@@ -34,16 +37,16 @@ func TestSetUpdateGet(t *testing.T) {
 	s := server.NewServer()
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
-	res := client.Set("a>", "b")
+	res := client.Set("a>", "b", 10 * time.Second)
 	res = client.Get("a>")
 	if res.Err != nil || res.Result != "b" {
-		t.Errorf("fail to get a key that was set", res)
+		t.Error("fail to get a key that was set", res)
 	}
-	res = client.Update("a>", "bb")
+	res = client.Update("a>", "bb", 10 * time.Second)
 	res = client.Get("a>")
 
 	if res.Result != "bb" || res.Err != nil {
-		t.Errorf("fail to get a key that was set then updated", res)
+		t.Error("fail to get a key that was set then updated", res)
 	}
 	s.Stop()
 }
@@ -53,16 +56,16 @@ func TestSetUpdateGetMap(t *testing.T) {
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
 	value := make(map[string]string)
-	client.Set("key", value)
+	client.Set("key", value, 0)
 	res := client.Get("key")
 	if !reflect.DeepEqual(value, res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set then updated", res)
+		t.Error("fail to get a key that was set then updated", res)
 	}
 	value["a"] = "b"
-	client.Update("key", value)
+	client.Update("key", value, 0)
 	res = client.Get("key")
 	if !reflect.DeepEqual(value, res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set then updated", res)
+		t.Error("fail to get a key that was set then updated", res)
 	}
 	s.Stop()
 }
@@ -72,16 +75,16 @@ func TestSetUpdateGetArray(t *testing.T) {
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
 	value := make([]string, 1)
-	client.Set("key", value)
+	client.Set("key", value, 0)
 	res := client.Get("key")
 	if !reflect.DeepEqual(value, res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set then updated", res)
+		t.Error("fail to get a key that was set then updated", res)
 	}
 	value[0] = "b"
-	client.Update("key", value)
+	client.Update("key", value, 0)
 	res = client.Get("key")
 	if !reflect.DeepEqual(value, res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set then updated", res)
+		t.Error("fail to get a key that was set then updated", res)
 	}
 	s.Stop()
 }
@@ -92,13 +95,13 @@ func TestDelete(t *testing.T) {
 	client, _ := NewClient("localhost:6380")
 	res := client.Delete("key")
 	if res.Err == nil {
-		t.Errorf("can't delete unxisted key with success")
+		t.Error("can't delete unxisted key with success")
 	}
-	client.Set("a", "b")
+	client.Set("a", "b", 0)
 	client.Delete("a")
 	res = client.Get("a")
 	if res.Err == nil {
-		t.Errorf("can't get deleted key with success")
+		t.Error("can't get deleted key with success")
 	}
 	s.Stop()
 }
@@ -107,15 +110,15 @@ func TestKeys(t *testing.T) {
 	s := server.NewServer()
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
-	client.Set("a", ">b")
-	client.Set("abc", ">abc")
-	client.Set("ab", ">bb")
+	client.Set("a", ">b", 0)
+	client.Set("abc", ">abc", 0)
+	client.Set("ab", ">bb", 0)
 
 	res := client.Keys("ab+")
 	expect := []string{"ab", "abc"}
 	sort.Strings(res.Result.([]string))
 	if res.Err != nil || !reflect.DeepEqual(expect, res.Result) {
-		t.Errorf("keys was not filtered by regex")
+		t.Error("keys was not filtered by regex")
 	}
 
 	s.Stop()
@@ -125,13 +128,13 @@ func TestCount(t *testing.T) {
 	s := server.NewServer()
 	s.Start(true)
 	client, _ := NewClient("localhost:6380")
-	client.Set("a", ">b")
-	client.Set("abc", ">abc")
-	client.Set("ab", ">bb")
+	client.Set("a", ">b", 0)
+	client.Set("abc", ">abc", 0)
+	client.Set("ab", ">bb", 0)
 
 	res := client.Count()
 	if res.Err != nil || res.Result != 3 {
-		t.Errorf("error with count", res)
+		t.Error("error with count", res)
 	}
 
 	s.Stop()
@@ -143,22 +146,22 @@ func TestSetUpdateGGetInArray(t *testing.T) {
 	client, _ := NewClient("localhost:6380")
 	value := make([]string, 1)
 	value[0] = "value1"
-	client.Set("key", value)
+	client.Set("key", value, 0)
 	res := client.GetIth("key", 0)
 	if !reflect.DeepEqual(value[0], res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set", res)
+		t.Error("fail to get a key that was set", res)
 	}
 
 	res = client.GetIth("key", 1)
 	if res.Err == nil || res.Err.Error() != "OOR" {
-		t.Errorf("OutOfRange was not fired", res)
+		t.Error("OutOfRange was not fired", res)
 	}
 
 	value2 := "value"
-	client.Set("key", value2)
+	client.Set("key", value2, 0)
 	res = client.GetIth("key", 0)
 	if res.Err == nil || res.Err.Error() != "WT" {
-		t.Errorf("WrongType was not fired", res)
+		t.Error("WrongType was not fired", res)
 	}
 
 	s.Stop()
@@ -171,22 +174,47 @@ func TestSetUpdateGGetInMap(t *testing.T) {
 	value := make(map[string]string, 1)
 	value1 := "value1"
 	value["key1"] = value1
-	client.Set("key", value)
+	client.Set("key", value, 0)
 	res := client.GetKeyInValue("key", "key1")
 	if !reflect.DeepEqual(value["key1"], res.Result) || res.Err != nil {
-		t.Errorf("fail to get a key that was set", res)
+		t.Error("fail to get a key that was set", res)
 	}
 	res = client.GetKeyInValue("key", "key2")
 	if res.Err == nil || res.Err.Error() != "NE" {
-		t.Errorf("'Not exist' was not fired", res)
+		t.Error("'Not exist' was not fired", res)
 	}
 
 	value2 := "value"
-	client.Set("key", value2)
+	client.Set("key", value2, 0)
 	res = client.GetKeyInValue("key", "key1")
 	if res.Err == nil || res.Err.Error() != "WT" {
-		t.Errorf("WrongType was not fired", res)
+		t.Error("WrongType was not fired", res)
 	}
 
 	s.Stop()
+}
+
+func BenchmarkBasic(b *testing.B) {
+	s := server.NewServer()
+	s.Start(true)
+	log.Println("start bench")
+	client, _ := NewClient("localhost:6380")
+
+	for n := 0; n < b.N; n++ {
+		setValues(&client, 1000000)
+	}
+	log.Println("stop bench")
+	s.Stop()
+}
+
+func setValues(client *Client, size int) {
+	for i := 0; i < size; i++ {
+		value := strconv.Itoa(i*2)
+		client.Set(strconv.Itoa(i), value, 1 * time.Second)
+		response := client.Get(strconv.Itoa(i))
+		if response.Result != value {
+			log.Print("fails in set get")
+		}
+	}
+
 }
